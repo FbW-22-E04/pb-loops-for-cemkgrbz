87f51b014a85f9ca859f4ba42324d146f53da66c
Cem <cemkgrbz@gmail.com>
2022-07-12 16:16:46 +0200

    added first 2 solutions

---
+++ solution.js
@@ -0,0 +1,20 @@
+//1 
+
+let sum = 0;
+for (let i = 0; i <= 20; i++) {
+    sum = sum + i;
+    console.log('The sum is', sum)
+}
+
+//2
+
+for (let i = 1; i <= 5; i++) {
+    if (i > 1) {
+    console.log(`There are ${i} bottle of beer on the table.`)
+    } else {
+    console.log(`There is 1 bottle of beer on the table.`)  
+    }
+}
+
+//3
+
